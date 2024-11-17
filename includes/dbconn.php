5d4ef94c04483b9064cc013fa70edc235fdd82c9
Manish Jadhav <manishsj289@gmail.com>
2024-11-17 18:29:10 +0530

    ..

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,7 +1,7 @@
-
 <?php
-$con = mysqli_connect("localhost", "root", "", "hostelmsphp");
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+    $dbuser="root";
+    $dbpass="";
+    $host="localhost";
+    $db="hostelmsphp";
+    $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
 ?>
